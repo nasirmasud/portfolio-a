19b14414f3d8b05e6238cc384e18a7c170212e28
Nasir Masud <nasir.masud@ymail.com>
2025-04-03 23:47:55 +0600

    Refactor Home component to include NavBar, Footer, Contact, and Services components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import KeyMetrics from "@/components/KeyMetrics";
+import LogoAnimation from "@/components/LogoAnimation";
+import NavBar from "@/components/NavBar";
+import Portfolio from "@/components/Portfolio";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <>
+      <NavBar />
       <Hero />
+      <LogoAnimation />
       <About />
+      <Portfolio />
+      <KeyMetrics />
+      <Services />
+      <Contact />
+      <Footer />
     </>
   );
 }
