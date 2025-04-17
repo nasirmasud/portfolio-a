696b08bb48202e1b52261ce5d0683cfb263d52e8
Nasir Masud <nasir.masud@ymail.com>
2025-04-17 12:08:56 +0600

    Update NavBar component: adjust layout for mobile and desktop, improve spacing, and enhance text size for better usability

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -26,20 +26,22 @@ export default function NavBar() {
   };
 
   return (
-    <div className='z-50 fixed flex justify-center w-full text-white font-bold'>
-      <NavContact />
+    <div className='z-50 fixed flex w-full text-white font-bold px-4 sm:px-8'>
+      <div className='hidden sm:block'>
+        <NavContact />
+      </div>
       <motion.div
-        className='bg-transparent mt-0 mr-8 hidden md:flex items-center justify-center p-2 max-w-[500px] ml-auto'
+        className='bg-transparent mt-0 hidden md:flex items-center justify-end p-2 w-full max-w-[500px] ml-auto'
         initial={{ opacity: 0, x: 20 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6, delay: 0.2 }}
       >
-        <ul className='flex flex-row p-2 space-x-8'>
+        <ul className='flex flex-row p-2 space-x-6'>
           {navLinks.map((link, index) => (
             <li key={index}>
               <Link
                 href={link.path}
-                className='transform hover:text-white/50 transition-all duration-300 ease-in-out'
+                className='transform hover:text-white/50 transition-all duration-300 ease-in-out text-sm'
               >
                 {link.title}
               </Link>
@@ -50,20 +52,25 @@ export default function NavBar() {
 
       <div
         onClick={toggleNav}
-        className='md:hidden absolute top-5 right-14 border rounded z-50 text-white/70 border-white/70 p-2'
+        className='md:hidden fixed top-4 right-4 border rounded z-50 text-white/70 border-white/70 p-2'
       >
-        {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
+      {/* Mobile menu */}
       <div
         className={`fixed left-0 top-0 w-full h-full bg-black/90 transform transition-transform duration-300 ${
           nav ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <ul className='flex flex-col items-center justify-center space-x-8 h-full'>
+        <ul className='flex flex-col items-center justify-center space-y-8 h-full'>
           {navLinks.map((link, index) => (
             <li key={index}>
-              <Link href={link.path} className='text-4xl' onClick={closeNav}>
+              <Link
+                href={link.path}
+                className='text-2xl block py-2'
+                onClick={closeNav}
+              >
                 {link.title}
               </Link>
             </li>
